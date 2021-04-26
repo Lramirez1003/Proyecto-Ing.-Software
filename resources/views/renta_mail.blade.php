498c92d23a29072c7608a82282c5a63fa41bfad9
Luis Angel Ramirez <20180570@ce.pucmm.edu.do>
2021-04-26 09:46:24 -0400

    otro arreglito

--- resources/views/renta_mail.blade.php
+++ resources/views/renta_mail.blade.php
@@ -1,11 +1,11 @@
 Hola {{ $name }}!,<br><br>
 
-Rentaste <b>{{$rent}}</b>.<br><br>
+Rentaste un <b>{{$rent}}</b>.<br><br>
 
 El precio total es <b> {{$prec}}</b>, y tu codigo de la renta es <b>{{$codigoderenta}}</b> <br><br>
 
-Con fecha de salida <b>{{$fechasalida}} </b>, hasta el <b>{{$fechaentrega}}</b><br><br>
+Con fecha de salida <b>{{\Carbon\Carbon::parse($fechasalida)->format('d-m-Y')}} </b>, hasta el <b>{{\Carbon\Carbon::parse($fechaentrega)->format('d-m-Y')}}</b><br><br>
 
-Gracias por tu renta, y si tienes alguna pregunta, no dudes en llamarnos al número: 809-724-6519.</div><br>
+Gracias por tu renta, y si tienes alguna pregunta, no dudes en llamarnos al número: 809-724-6519.</div><br><br>
 
 Victor Ureña
